6dd78dde3b234cd9939e8f273817d8218f443705
rizarayhan <rizarayhanw@gmail.com>
2025-10-31 19:54:51 +0700

    update dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,6 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import { prisma } from "@/lib/db";
 
 interface DashboardPageProps {
@@ -5,13 +8,61 @@ interface DashboardPageProps {
 }
 
 const DasboardPage = async ({ params }: DashboardPageProps) => {
-  const store = await prisma.store.findFirst({
+  const banners = await prisma.banner.findMany({
     where: {
-      id: params.storeId,
+      storeId: params.storeId,
     },
   });
 
-  return <div>Active Store = {store?.name}</div>;
+  const categories = await prisma.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  const products = await prisma.product.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading
+          title="Dashboard"
+          description="Ringkasan Toko"
+        />
+        <Separator />
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total Banner</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{banners.length}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total Category</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{categories.length}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total Product</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{products.length}</div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DasboardPage;
